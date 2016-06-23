df11b4a31ec5d7411289deb3aa5a4dea4b30e623
Anish Tuli <tulianish@gmail.com>
2016-06-23 14:22:42 +0530

    Added mail to tulianish feature + Thanks page

--- mail.php
+++ mail.php
@@ -1,10 +1,38 @@
 <?php
-// the message
-$msg = "First line of text\nSecond line of text";
+if($_POST){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $msg = $_POST['message'];
 
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
+//send email
+$to = "tulianish@gmail.com";
 
-// send email
-mail("tulianish@gmail.com","Test",$msg);
+$subject = 'Rubaroo Response';
+$from = $email;
+
+ 
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Create email headers
+$headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+ 
+// Compose a simple HTML email message
+$msg1 = '<html><body>';
+$msg1 .= '<h3> From : '.$name.'</h3>';
+$msg1 .= '<h3> Email : '.$email.'</h3>';
+$msg1 .= '<h3>'.$msg.'</h3>';
+$msg1 .= '</body></html>';
+ 
+// Sending email
+if(mail($to, $subject, $msg1, $headers)){
+	header('Location: https://srmrubaroo-anishtuli.rhcloud.com/thank.html');
+} else{
+    echo 'Unable to send email. Please try again later.';
+}}
+
+
 ?>
