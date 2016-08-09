4216b2b1113449edb921ee603a58395426393465
Anish Tuli <tulianish@gmail.com>
2016-08-09 23:57:18 +0530

    Committee + Home fixes

--- mail.php
+++ mail.php
@@ -1,22 +1,22 @@
 <?php
 
 error_reporting(0);
-$conn=mysql_connect("127.12.107.2","adminC2gn4Nh","2VxHrSns5Zaa");
+$conn=mysql_connect("localhost","srmrubar_admin","srm1234");
 if(! $conn )
    {
      die('Could not connect: ' . mysql_error());
    }
-   echo 'Connected successfully';
-$sel=mysql_select_db("srmrubaroo");
+   //echo 'Connected successfully';
+$sel=mysql_select_db("srmrubar_contact");
 if(! $sel )
    {
      die('Could not connect: ' . mysql_error());
    }
-    echo 'DATABASE successfully'; 
+    //echo 'DATABASE successfully'; 
 
 if(isset($_POST['submit']))
 {
-	$date=date("Y-m-d");
+	$date=date("Y-m-d H:i:s");
 	$sql = "INSERT INTO enquiry (name,email,message,datetime)
 VALUES ('".$_POST['name']."', '".$_POST['email']."','".$_POST['message']."','".$date."')";  
 mysql_query($sql);
@@ -34,8 +34,6 @@ $to = "tulianish@gmail.com";
 $subject = 'Rubaroo Response';
 $from = $email;
 
- 
-// To send HTML mail, the Content-type header must be set
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
  
@@ -44,19 +42,19 @@ $headers .= 'From: '.$from."\r\n".
     'Reply-To: '.$from."\r\n" .
     'X-Mailer: PHP/' . phpversion();
  
-// Compose a simple HTML email message
+// Compose the HTML email message
 $msg1 = '<html><body>';
 $msg1 .= '<h3> From : '.$name.'</h3>';
 $msg1 .= '<h3> Email : '.$email.'</h3>';
 $msg1 .= '<h3>'.$msg.'</h3>';
 $msg1 .= '</body></html>';
- 
-// Sending email
+ // Sending email
 if(mail($to, $subject, $msg1, $headers)){
-	header('Location: https://srmrubaroo-anishtuli.rhcloud.com/thank.html');
+	header('Location: http://srmrubaroo.com/thank.html');
 } else{
     echo 'Unable to send email. Please try again later.';
 }}
+
 
 
 
