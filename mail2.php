ec118534519eb4d6e0a6cc4a36d714d6f70fd29f
Dantin Kakkar <dantinkakkar@Dantins-MacBook-Air.local>
2016-08-19 01:22:37 +0530

    Reg changes, home bg change

---
+++ mail2.php
@@ -0,0 +1,39 @@
+<?php
+
+
+
+
+if($_POST){
+    $name = $_POST['email_reg'];
+
+//send email
+$to = "tulianish@gmail.com";
+
+$subject = 'Rubaroo Response';
+$from = $email;
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Create email headers
+$headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+// Compose the HTML email message
+$msg1 = '<html><body>';
+$msg1 .= '<h3> Email : '.$email.'</h3>';
+$msg1 .= '<h3>Interest in external registrations</h3>';
+$msg1 .= '</body></html>';
+ // Sending email
+if(mail($to, $subject, $msg1, $headers)){
+	header('Location: http://srmrubaroo.com/thank.html');
+} else{
+    echo 'Unable to send email. Please try again later.';
+}}
+
+
+
+
+
+?>
